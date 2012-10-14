b581b41884baefdd207b0b575dcc1584a09f0bcf
Phillip McCartney <pmccartney@ubuntu-VirtualBox.(none)>
2012-10-14 14:46:16 -0500

    New development script

---
+++ DevelopmentInstall.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+##############################
+#
+#  Script to install all development
+#
+##############################
+
+mkdir Development
+
+
+echo "---------------- Installing Node.js"
+chmod +x InstallNode.sh
+cd Development
+./../InstallNode.sh
+
+
